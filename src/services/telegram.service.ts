555809485596d03ed96e66fb5d7cebd1e7783d9e
Oscar Cedano <cedano@engineer.com>
2026-04-15 02:33:19 -0600

    wip: telegram service

--- src/services/telegram.service.ts
+++ src/services/telegram.service.ts
@@ -1,3 +1,4 @@
+import { Template } from "@/entities/template.entity";
 import { TelegramMessage } from "@/models/telegram.model";
 import { injectable, singleton } from "tsyringe";
 
@@ -8,5 +9,16 @@ export default class TelegramService {
         return {
             telegram_message_id: "1234567890"
         }
+    }
+
+    public async sendBunchOfMessages(_templates: (Pick<Template, "id" | "channel_id" | "content"> & { chat_id: string })[]): Promise<TelegramMessage[]> {
+        const messages: TelegramMessage[] = [];
+
+        for (const template of _templates) {
+            const message = await this.sendMessage(template.chat_id, template.content);
+            messages.push(message);
+        }
+
+        return messages;
     }
 }
