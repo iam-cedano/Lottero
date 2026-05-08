3fb9e93bbfd20be1b7e2d0993e4cc2b9c85fa68f
Oscar Cedano <cedano@engineer.com>
2026-05-08 01:44:27 -0600

    fix: 'created' column from 'group_messages' table is not storing time

--- src/controllers/message.controller.ts
+++ src/controllers/message.controller.ts
@@ -1,6 +1,6 @@
 import BaseException from "@/exceptions/base.exception";
 import ValidationException from "@/exceptions/validation.exception";
-import { EditMessageGroupRequest, MessageRequest } from "@/models/message.model";
+import { MessageRequest, EditMessageRequest } from "@/models/message.model";
 import MessageService from "@/services/message.service";
 import { Request, Response } from "express";
 import { inject, injectable } from "tsyringe";
@@ -45,14 +45,14 @@ export default class MessageController {
     };
 
     public editMessage = async (
-        req: Request<Record<string, string>, unknown, EditMessageGroupRequest>,
+        req: Request<Record<string, string>, unknown, EditMessageRequest>,
         res: Response,
     ) => {
         try {
-            const { type } = req.body;
+            const { data } = req.body;
             const { groupMessageId } = req.params;
 
-            if (!type || type.trim() == "") {
+            if (!data.type || data.type.trim() == "") {
                 throw new ValidationException("Data type is required and cannot be empty.");
             }
 
@@ -60,7 +60,7 @@ export default class MessageController {
                 throw new ValidationException("Group message ID is required and must be a valid number.");
             }
 
-            const result = await this.messageService.editMessage(Number(groupMessageId), req.body);
+            const result = await this.messageService.editMessage(Number(groupMessageId), data);
 
             res.status(200).send(result);
         } catch (error) {
